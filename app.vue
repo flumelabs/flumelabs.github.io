ba9c15fe9f37e055bce674bd8be8477c6967b34d
Nidhish Shah <nidhish.shah@prosus.com>
2024-09-29 18:42:18 +0200

    feat: initial website implementation.

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="flex justify-center items-center h-screen bg-amber-50">
+    <section class="w-full max-w-screen-md p-3">
+      <h1 class="text-5xl font-bold">flume labs.</h1>
+      <p class="text-2xl text-muted-foreground mb-5">/fluːm læbz/</p>
+      <p class="text-xl font-mono">We craft tools that awaken the flow state within us all — channeling creativity and focus into a seamless stream of productivity.</p>
+    </section>
+  </main>
+</template>
